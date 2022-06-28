2808e34f8979b8f86deb5d5a0ef686bf455425dc
koztimesin <a.isumairu01@gmail.com>
2022-06-28 13:15:58 +0300

    first commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Karthus Saffron on 6/28/22.
+//  Copyright © 2022 koztimesin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
